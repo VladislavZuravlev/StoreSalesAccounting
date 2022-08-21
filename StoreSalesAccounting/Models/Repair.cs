b9025d0e4ec45b3ed6f3e79d3048dd54c3d727a9
zuravlevvladislav19@gmail.com <zuravlevvladislav19@gmail.com>
2022-08-21 14:23:26 +0300

    Created new model "Repair"

---
+++ StoreSalesAccounting/Models/Repair.cs
@@ -0,0 +1,17 @@
+﻿namespace CashFlowWeb.Models
+{
+    public class Repair
+    {
+        public int RepairId { get; set; }
+        public DateTime RepairStartDate { get; set; }
+        public string ReceivingEmployee { get; set; }
+        public string Master { get; set; }
+        public string ClientName { get; set; }
+        public int ClientNumber { get; set; }
+        public string MusicalInstrument { get; set; }
+        public string TechnicalTask { get; set; }
+        public bool GuitarCase { get; set; }
+        public DateTime RepairEndDate { get; set; }
+        public bool GiveAway { get; set; }
+    }
+}
